9ea6d6fea051e16a71607c907f2b4d85a71a59a2
SergeStr <sergestrizhov@gmail.com>
2024-09-26 15:13:23 -0700

    simple shell finished: now need to do writeup

--- strizhov_serge_HW3_main.c
+++ strizhov_serge_HW3_main.c
@@ -1,14 +1,16 @@
 /**************************************************************
-* Class::  CSC-415-0# Summer 2024
-* Name::
-* Student ID::
-* GitHub-Name::
+* Class::  CSC-415-03 Fall 2024
+* Name:: Serge Strizhov
+* Student ID:: 922426905
+* GitHub-Name:: SergeStr
 * Project:: Assignment 3 – Simple Shell with Pipes
 *
-* File:: <name of this file>
+* File:: strizhov_serge_HW3_main.c
 *
-* Description::
-*
+* Description:: Runs a unique simple shell inside of the terminal.
+* Takes in user input, parses them as functions and their arguments
+* and then executes. This simple shell supports piping, up until
+* the command line limit of 159 bytes.
 **************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
@@ -18,76 +20,138 @@
 #include <sys/wait.h>
 
 int main(int argc, char* argv[]) {
+    // 159 due to it being the limit of the command line
+    // input.
     char* buffer = malloc(sizeof(char)*159);
+    // Stores the original buffer location, due
+    // to the buffer being constantly modified by
+    // strtok_r(). Otherwise, results in a free()
+    // error.
+    char* originalBufferLocation = buffer;
+    // Failsafe for whenever buffer doesn't malloc properly.
     if(buffer == NULL) {
         printf("Buffer failed to malloc. Exiting.\n");
     }
+    // Sets up a placeholder prompt, which is overriden
+    // by a given prompt if it exists.
     char* prompt = "> ";
     if (argc > 1) {
         prompt = argv[1];
     }
 
+    // I like to terminate my user-oriented loops
+    // By setting the working value to 0 and then
+    // continuing, instead of breaking out.
     int working = 1;
     while (working) {
         printf("%s", prompt);
-
+        // fgets is used as it can collect up to an X of 
+        // byte data. scanf isn't very good at this.
         char* result = fgets(buffer, sizeof(char)*159, stdin);
-        // Error and exit checks as well as outputs.
-        if(result == NULL){
+        // Error and exit checks.
+        if(result == NULL){ // Occurs on EOF
             working = 0;
             continue;
         }
+        // Otherwise gets valid data, from which it removes 
+        // newline character at the end that
+        // fgets likes to take.
         else {
             if ((strlen(result) > 0) && (result[strlen(result)-1] == '\n')){
                 result[strlen(result)-1]='\0';
             }
         }
-        
+        // Some predetermined data states and their results.
         if(strlen(buffer) == 0) {
             printf("Error: No input. Please enter a command.\n");
             continue;
         }
         else if (strcmp(buffer, "exit") == 0) {
-            printf("Thank you for using this Simple Shell. Exiting.\n");
             working = 0;
             continue;
         } 
-        else {
-            // Sweeping the buffer without modifying it.
-            char* bufferLoc = strchr(buffer, ' ');
-            // To account for null term and no space at the end,
-            // This starts at two.
+        // Otherwise, most likely valid funcs. 
+        // Sets up forking, piping, and passing
+        // data between loops.
+        pid_t exe;
+        int pi[2];
+        int data = 0;
+        char* bufferLoc;
+        // Splits up input by pipes to then do one by one.
+        char* func = strtok_r(buffer, "|", &buffer);
+        while (func != NULL) {
+            // In the case of pipes, there
+            // can be spaces before or after
+            // the func. This removes the
+            // 'before'.
+            bufferLoc = strchr(func, ' ');
+            if (bufferLoc == func) {
+                bufferLoc++;
+            }
+            // Checks for the amount of spaces, so
+            // it knows how big the argv list
+            // should be.
             int countOfArgs = 2;
             while (bufferLoc != NULL){
                 countOfArgs++;
-                bufferLoc = bufferLoc + 1;
+                bufferLoc++;
                 bufferLoc = strchr(bufferLoc, ' ');
             }
-            // Sweeping the buffer again and this time
-            // Modifying via tokenization.
+            // Removes the 'after' space
+            if (func[strlen(func)-1] == ' ') {
+                countOfArgs--;
+            }
+ 
+            // Setting up and population of argv
             char* args[countOfArgs];
-            char* execName;
-            char* arg = strtok(buffer, " ");
-            execName = arg;
+            char* arg = strtok(func, " ");
+            char* execName = arg;
             for(int i = 0; i < countOfArgs-1; i++) {
                 args[i] = arg;
                 arg = strtok(NULL, " ");
             }
+            // Null termination for argv as its required
             args[countOfArgs-1] = NULL;
 
-            pid_t exe = fork();
-            if (exe < 0) {
-                printf("Error making fork! Terminating.\n");
+            // Sets up the next func for future loop.
+            func = strtok_r(buffer, "|", &buffer);
+
+            pipe(pi);
+            exe = fork();
+
+            // Terminates if the fork fucked up.
+            if ((exe < 0)) {
+                printf("Forking error. Terminating.\n");
                 working = 0;
                 continue;
             }
-            else if (exe == 0){
+            // Executes the func with its func arg given.
+            // IF there are future funcs (pipes) then
+            // Stores STDOUT in 'data' variable
+            // for next func to take it as STDIN
+            // Also takes data outputted by prev
+            // func and uses it as STDIN.
+            else if (exe == 0) {
+                close(pi[0]);
+                dup2(data, STDIN_FILENO);
+                if (func != NULL) {
+                    dup2(pi[1], 1);
+                }
+                close(pi[1]);
                 execvp(execName, args);
-                return 1; //Failsafe in case execvp doesn't do anything.
+                // This should never happen. Hopefully.
+                printf("Error executing instructions. Terminating.\n");
+                return 1;
             }
+            // Prints data of child process
+            // such as its PID and execution result.
+            // Closes its STDIN pipe if there are no
+            // future funcs.
             else {
                 int result;
                 waitpid(exe, &result, 0);
+                close(pi[1]);
+                data = pi[0];
                 if (WIFEXITED(result)) {
                     printf("Child %d, exited with %d\n", exe, WEXITSTATUS(result));
                 }
@@ -96,10 +160,14 @@ int main(int argc, char* argv[]) {
                     working = 0;
                     continue;
                 }
+
+                if (func == NULL){
+                    close(pi[0]);
+                }
             }
         }
     }
 
-    free(buffer);
+    free(originalBufferLocation);
     return 0;
 }
